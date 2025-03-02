ef9ec5b66a49fc4fae32ea8e20d02dbf8f118d19
shimizu.kenji <shimizu.kenji@noncore.net>
2025-03-02 16:27:31 +0900

    feat: preload script

--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron
-  // 関数だけでなく変数も公開できます
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
 })
